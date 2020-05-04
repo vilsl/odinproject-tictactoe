c947a39b1a6aa193c15e1d9c23bf7915c29725e7
vilsl <vilsl@fastmail.com>
2020-05-04 13:57:33 +0200

    Now renders a game board on a HTML page, from an array.

--- tictactoe.js
+++ tictactoe.js
@@ -1,3 +1,47 @@
 /*
 Tic tac toe. Homework from The Odin Project. Do what you want with it. -vilsl
 */
+
+// Module that handles board logic
+const GameBoard = (() => {
+    let board = [
+        'x','o','x',
+        'x','x','o',
+        'x','o','x',
+    ]
+
+    return {
+        board,
+    }
+
+})(); 
+
+// Outputs board and UI to HTML
+const DisplayController = (() => {
+    const renderBoard = () => {
+        let board = GameBoard.board;
+        for (let i = 0; i < board.length; i++){
+            document.getElementById(`tile${i}`).innerHTML = board[i];
+        }
+    }
+
+    return {
+        renderBoard,
+    };
+
+})();
+
+// Calls upon appropriate functions to run game, reset functionality
+const Game = (() => {
+    DisplayController.renderBoard();
+})();
+
+// Player objects
+const Player = (name) => {
+    const getName = () => name;
+    const wins = () => wins;
+    const piece = () => piece;
+
+    return {getName, wins, piece}
+};
+
